3ce6bb3bfb2c6f57abbdc3a24fb757fd8cc45dfe
MamoonUddin <Mamoonuddin17@gmail.com>
2023-08-08 02:55:42 -0500

    fixes

--- src/component/ContactForm/ContactForm.js
+++ src/component/ContactForm/ContactForm.js
@@ -9,7 +9,7 @@ export default function ContactForm() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm("service_0jiqbb9","template_z37vwud", form.current,"gBpGvMErweIkkpNfH")
+    emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID ,process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current,process.env.REACT_APP_EMAILJS_PUBLIC_KEY      )
       .then(() => {
           form.current.reset();
           
